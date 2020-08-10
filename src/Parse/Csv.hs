9287f6d2ba60c4fe62d72707f0f88c9d55349f33
Sam Balco <goodlyrottenapple@gmail.com>
2020-08-10 16:26:57 +0100

    can parse CSV with ,/; delimiters. Auto-detects which one....in theory

--- src/Parse/Csv.hs
+++ src/Parse/Csv.hs
@@ -1,9 +1,11 @@
 module Parse.Csv(readCsvFile) where
 
+import           Data.Char                  (ord)
 import           Data.Text                  (Text)
 import qualified Data.ByteString.Lazy       as BS
 import qualified Data.Aeson                 as JSON
-import           Data.Csv.Streaming         (Records(..), HasHeader(..), decode)
+import           Data.Csv.Streaming         (Records(..), HasHeader(..), decode, decodeWith)
+import           Data.Csv.Parser            (defaultDecodeOptions, decDelimiter)
 import qualified Data.HashMap.Strict        as HM
 import           Control.Monad.Catch        (MonadThrow(..))
 import           Control.Monad.IO.Class     (MonadIO, liftIO)
@@ -16,11 +18,27 @@ instance TraversableWithIndex Int Records
 instance FunctorWithIndex Int Records
 
 readCsvFile :: (MonadThrow m, MonadIO m) => FilePath -> m ([JSON.Value], Records (HM.HashMap Text JSON.Value))
-readCsvFile inFile = do
+readCsvFile inFile = readCsvFileWithDelimiter [',', ';'] inFile
+
+
+-- Try parsing a CSV file with ',' as the delimiter. If we encounter only one header, it is likely that a different separator was used.
+-- In that case, try parsing the file again with ';' instead. If we still get only one header, revert to default, i.e. parsing with ','.
+readCsvFileWithDelimiter :: (MonadThrow m, MonadIO m) => [Char] -> FilePath -> m ([JSON.Value], Records (HM.HashMap Text JSON.Value))
+readCsvFileWithDelimiter [] inFile = do
   rows <- liftIO $ decode NoHeader <$> BS.readFile inFile
   case rows of
+    Cons (Right (h :: [Text])) rest -> let header = uniqueHeader h in 
+      return (map (uncurry unifyHeader) $ zip header h, fmap (\r -> HM.fromList $ zip header $ map JSON.toJSON r) rest)
+    Cons (Left e) _ -> throwM $ CSVParseError e
+    Nil (Just e) _ -> throwM $ CSVParseError e
+    Nil Nothing r -> return ([], Nil Nothing r)
+readCsvFileWithDelimiter (delim:alts) inFile = do
+  rows <- liftIO $ decodeWith defaultDecodeOptions{ decDelimiter = fromIntegral (ord delim) } NoHeader <$> BS.readFile inFile
+  case rows of
     Cons (Right (h :: [Text])) rest -> 
-      let header = uniqueHeader h in return (map (uncurry unifyHeader) $ zip header h, fmap (\r -> HM.fromList $ zip header $ map JSON.toJSON r) rest)
+      if length h < 2 then readCsvFileWithDelimiter alts inFile
+      else let header = uniqueHeader h in 
+        return (map (uncurry unifyHeader) $ zip header h, fmap (\r -> HM.fromList $ zip header $ map JSON.toJSON r) rest)
     Cons (Left e) _ -> throwM $ CSVParseError e
     Nil (Just e) _ -> throwM $ CSVParseError e
     Nil Nothing r -> return ([], Nil Nothing r)
